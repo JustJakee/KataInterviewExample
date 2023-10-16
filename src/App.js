40c451a953bffe3adf14152c732ab729fe742436
Jake Bertish <bertishprofessional@gmail.com>
2023-10-16 14:27:29 -0500

    Add code from Interview and update view.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
 import './App.css';
-import logo from './logo.svg'
+import { useState } from 'react'
 
 function App() {
+  const [temp, updateTemp] = useState({ far: 0, cel: 0 });
+  //Attempted to create saved temperatures 
+  //const [savedTemps, updateSavedTemps] = useState([{}])
+
+  const updateCel = e => updateTemp({
+    far: (e.target.value * 9 / 5) + 32,
+    cel: e.target.value,
+  })
+
+  const updateFar = e => updateTemp({
+    far: e.target.value,
+    cel: (e.target.value - 32) * 5 / 9
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <header className="App-header">
+      <div className="App">
+        <h1>Jake Bertish - Calculator</h1>
+        <h2>Celsius</h2>
+        <input
+          class="input-box"
+          type="text"
+          value={temp.cel}
+          onChange={updateCel}
+        ></input>
+        <h2>Farenheit</h2>
+        <input
+          class="input-box"
+          type="text"
+          value={temp.far}
+          onChange={updateFar}
+        ></input>
+      </div>
+    </header>
   );
 }
 
